781917301024a9b002b9a71f288044622d218c6c
Alberto Herrera <albherre@c2r10s4.42luxembourg.lu>
2024-03-28 22:47:58 +0100

    2nd upload of printf_project

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -1,41 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: albherre <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/20 16:18:40 by albherre          #+#    #+#             */
-/*   Updated: 2024/03/06 13:29:37 by albherre         ###   ########.fr       */
+/*   Created: 2024/03/26 16:24:39 by albherre          #+#    #+#             */
+/*   Updated: 2024/03/26 16:24:41 by albherre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-void	*ft_memmove(void *dest, const void *src, size_t n)
-{
-	const unsigned char	*s;
-	unsigned char		*d;
-
-	s = (unsigned char *) src;
-	d = (unsigned char *) dest;
-	if (d == s)
-		return (dest);
-	if (d < s)
-	{
-		while (n--)
-		{
-			*d++ = *s++;
-		}
-	}
-	else
-	{
-		s += n;
-		d += n;
-		while (n--)
-		{
-			*--d = *--s;
-		}
-	}
-	return (dest);
-}
+# include "libft/libft.h"
